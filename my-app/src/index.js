5622bf923688ce3e95fb766b991db751b9c89134
EdisonIm <alicparadigm@gmail.com>
2023-12-12 20:25:02 +0900

    시계만들기

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,12 +6,16 @@ import reportWebVitals from './reportWebVitals';
 
 import Library from './chapter03/library';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Library />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import Clock from './chapter04/clock';
+
+setInterval(() => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Clock />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
