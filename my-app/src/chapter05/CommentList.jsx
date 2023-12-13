dc7cf671a3bf424292671ab6414d343b93cea977
EdisonIm <alicparadigm@gmail.com>
2023-12-13 23:41:33 +0900

    chapter05_comment data

--- my-app/src/chapter05/CommentList.jsx
+++ my-app/src/chapter05/CommentList.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 import Comment from "./Comment";
 
+const comments = [
+    {
+        name: "남태호",
+        comment: "안녕하세요. 고통 담당자입니다.",        
+    },
+    {
+        name: "전정호",
+        comment: "와이어프레임 재미있어요~!",
+    },
+    {
+        name: "임병성",
+        comment: "제가 개발을 하고 있어요!!!",
+    },
+];
+
+
 function CommentList(props) {
     return (
         <div>
-            <Comment />
+            {comments.map((comment) => {
+                return (
+                    <Comment name={comment.name} comment={comment.comment} />                    
+                );
+            })}            
         </div>
     );
 }
