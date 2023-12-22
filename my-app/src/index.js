735f65e3406b21f6ea0821da6b2df7998b004736
EdisonIm <alicparadigm@gmail.com>
2023-12-22 18:27:55 +0900

    chapter08_ConfirmButton(Bind)

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import Library from './chapter03/library';
-import Clock from './chapter04/clock';
-import CommentList from './chapter05/CommentList';
-import NotificationList from './chapter06/NotificationList';
-import Accomodate from './chapter07/Accomodate';
+// import Library from './chapter03/library';
+// import Clock from './chapter04/clock';
+// import CommentList from './chapter05/CommentList';
+// import NotificationList from './chapter06/NotificationList';
+// import Accomodate from './chapter07/Accomodate';
+import ConfirmButton from './chapter08/ConfirmButton';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Accomodate />
+    <ConfirmButton />
   </React.StrictMode>,
   document.getElementById('root')
 );
