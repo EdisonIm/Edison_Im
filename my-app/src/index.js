d06158b377c3b9218acfd9c3b9f9f8bcfaab88e0
EdisonIm <alicparadigm@gmail.com>
2023-12-26 14:44:59 +0900

    chapter14_context

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -13,12 +13,13 @@ import reportWebVitals from './reportWebVitals';
 // import LandingPage from './chapter09/LandingPage';
 // import AttendanceBook from './chapter10/AttendanceBook';
 // import SignUp from './chapter11/SignUp';
-//import Calculator from './chapter12/Calculator';
-import ProfileCard from './chapter13/ProfileCard';
+// import Calculator from './chapter12/Calculator';
+// import ProfileCard from './chapter13/ProfileCard';
+import DarkOrLight from './chapter14/DarkOrLight';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ProfileCard />
+    <DarkOrLight />
   </React.StrictMode>,
   document.getElementById('root')
 );
